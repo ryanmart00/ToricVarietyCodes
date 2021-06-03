d666e550aa85d6162c2fc2df4545981c719c61de
Ryan Martinez <ryanmart00@gmail.com>
2021-06-03 12:32:52 -0700

    finished finite fields

--- fields.py
+++ fields.py
@@ -144,6 +144,12 @@ def Z(prime):
         def __req__(self, other):
             return self.__eq__(other)
 
+        def __ne__(self, other):
+            return not self.__eq__(other)
+
+        def __rne__(self, other):
+            return not self.__eq__(other)
+
     return Zp
 
 class PolyMeta(type):
@@ -163,14 +169,16 @@ def PolyOver(Ring, symbol='x'):
 
         @classmethod
         def set(cls, deg):
-            from itertools import product
-            
+            for i in range(cls.R.p ** (deg+1)):
+                poly = cls([cls.R(i // (cls.R.p ** j)) for j in range(deg+1)]) 
+                if poly.degree() == deg:
+                    yield poly
         
         def __new__(cls, vals):
             converted = [val if isinstance(val, Ring) else Ring(val)\
                     for val in vals]
             #trim high order zeros
-            while converted[-1] == 0:
+            while len(converted) > 1 and converted[-1] == 0:
                 converted = converted[:-1]
             return tuple.__new__(cls, converted)
 
@@ -253,6 +261,15 @@ def PolyOver(Ring, symbol='x'):
             return str(self)
 
         """
+            evaluate this polynomial
+        """
+        def __call__(self, x):
+            if not isinstance(x, Rx.R):
+                x = Rx.R(x)
+            return sum([self[i] * (x ** i) for i in range(len(self))])
+
+
+        """
             This uses the fact that polynomials over a field are a Euclidean Domain
             to find polynomials q, r such that 
 
@@ -298,12 +315,191 @@ def PolyOver(Ring, symbol='x'):
             except:
                 return NotImplemented
             return other.__mod__(self)
+
+        def __eq__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+            if len(self) != len(other):
+                return False
+            return all([self[i] == other[i] for i in range(len(self))])
+
+        def __req__(self, other):
+            return self.__eq__(other)
+
+        def __ne__(self, other):
+            return not self.__eq__(other)
+
+        def __rne__(self, other):
+            return not self.__eq__(other)
                 
 
     return Rx 
 
 """
+    This meta class gives correct type equality
+"""
+class FieldMeta(type):
+    def __repr__(cls):
+        return "F%i" % (cls.P ** cls.N)
+
+    def __eq__(cls, other):
+        if type(cls) == type(other):
+            return cls.P == other.P and cls.N == other.N
+        return False
+
+def getIrreducible(Poly, deg):
+    R = Poly.R
+    #Store lower order irreducibles
+    irreducibles = []
+    if not isinstance(deg, int) or deg < 2:
+        raise TypeError("No irreducible polynomials of degree %s" % deg)
+    for i in range(2, deg+1):
+        for f in Poly.set(i):
+            if all([f(x) != 0 for x in R.set()]):
+                # This polynomial has no zeros <=> Has no linear factors
+                if all([f % g for g in irreducibles]):
+                    # f has no irreducible factors either
+                    if i == deg:
+                        return f
+                    irreducibles += [f]
+    raise ValueError("We couldn't find an irreducible polynomial...")
+
+"""
     F(p,n, symbol='x') Generates the field of size p^n with symbol x as the ajoined symbol
 """
-def F(p,n, symbol='x'):
-    pass
+def F(prime,power, symbol='x'):
+    if power == 1:
+        return Z(prime)
+    if power < 1 or not isinstance(power,int):
+        raise TypeError("The power %s is not a natural number!" % str(power))
+    #First we find an irreducible polynomial of degree n
+    R = Z(prime)
+    Poly = PolyOver(R,symbol)
+    
+    ideal = getIrreducible(Poly, power)
+
+    class Fpn(tuple,metaclass=FieldMeta):
+        P = prime
+        N = power
+        S = symbol
+        I = ideal
+
+        @classmethod
+        def set(cls):
+            pass
+        
+        def __new__(cls, val):
+            if not isinstance(val, Poly):
+                val = Poly(val)
+            return tuple.__new__(cls, (val % Fpn.I,))
+
+        @classmethod
+        def id(cls):
+            return cls(cls.P(0))
+
+        @property
+        def val(self):
+            return self[0]
+
+        def _convert(self, other):
+            if type(self) == type(other):
+                return other
+            return type(self)(other)
+
+        def __add__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+            return Fpn(self.val + other.val)
+
+        def __radd__(self, other):
+            return self.__add__(other)
+
+        def __sub__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+            return Fpn(self.val - other.val)
+
+        def __rsub__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+            return other.__sub__(self)
+
+        def __neg__(self):
+            return Fpn(-self.val)
+
+        def __mul__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+
+            return Fpn(self.val * other.val)
+
+        def __rmul__(self, other):
+            return self.__mul__(other)
+
+        def __pow__(self, power):
+            if not isinstance(power, int):
+                raise TypeError("Cannot raise to non-integer power")
+            power = power % (Fpn.P ** Fpn.N - 1)
+            counter = Fpn([1])
+            for i in range(0,power): #This could be improved by splitting power up in binary
+                counter = counter * self
+            return counter
+
+        def __div__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+
+            if other.val == 0:
+                raise ZeroDivisionError()
+            #Since the multiplicative group is cyclic of size
+            #q-1, a** (q-2) * a = a ** (q-1) = 1
+            return self * (other ** ((Fpn.P ** Fpn.N) - 2)) 
+
+        def __rdiv__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+            return other.__div__(self)
+
+        __floordiv__ = __div__
+        __rfloordiv__ = __rdiv__
+
+        __truediv__ = __div__
+        __rtruediv__ = __rdiv__
+        
+        def __str__(self):
+            return str(self.val) 
+
+        def __repr__(self):
+            return str(self)
+
+        def __eq__(self, other):
+            try:
+                other = self._convert(other)
+            except:
+                return NotImplemented
+            return self.val == other.val;
+
+        def __req__(self, other):
+            return self.__eq__(other)
+
+        def __ne__(self, other):
+            return not self.__eq__(other)
+
+        def __rne__(self, other):
+            return not self.__eq__(other)
+
+    return Fpn
