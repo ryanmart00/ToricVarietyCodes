11bc8a9d4fa1372e09771a8f67226ef2b6e66fe7
Ryan Martinez <ryanmart00@gmail.com>
2021-06-30 10:42:52 -0700

    flush computes output

--- compute.py
+++ compute.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
 from codes import *
 from fields import *
 
-if __name__ == '__main__':
-    points = [(0, 0, 5), (4, 2, 0), (2, 3, 0),\
-        (2, 1, 0), (0, 0, 1), (2, 1, 1), (0, 0, 2), (2, 1, 2),\
-        (0, 0, 3), (0, 0, 4), (1, 1, 0), (3, 2, 0), (1, 1, 1),\
-        (1, 1, 2), (1, 1, 3), (2, 2, 0), (2, 2, 1) ]
-    f = F(7,1)
-    print("Computing min dist for %s over %s" % (str(points), str(f)))
-    print(CodeFromLatticePoints(f, points, True).d())
+points = [(0, 0, 5), (4, 2, 0), (2, 3, 0),\
+    (2, 1, 0), (0, 0, 1), (2, 1, 1), (0, 0, 2), (2, 1, 2),\
+    (0, 0, 3), (0, 0, 4), (1, 1, 0), (3, 2, 0), (1, 1, 1),\
+    (1, 1, 2), (1, 1, 3), (2, 2, 0), (2, 2, 1) ]
+f = F(7,1)
+print("Computing min dist for %s over %s" % (str(points), str(f)), flush=True)
+print(CodeFromLatticePoints(f, points, True).d(), flush=True)
